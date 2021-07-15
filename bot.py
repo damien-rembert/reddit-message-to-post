74987c15ce567e0c6054b25fb74f6bac0a7be79a
Damien Rembert <damien.rembert@gmail.com>
2021-07-15 17:02:09 +0100

    adding sender is mod for admin bits

--- bot.py
+++ bot.py
@@ -5,13 +5,14 @@ import os
 import re
 
 
-# TODO automatically mark mods from ***REMOVED*** as Trusted
-# TODO replace ***REMOVED*** with ***REMOVED***
 # TODO set karma
+# TODO replace ***REMOVED*** with ***REMOVED***
+# TODO automatically mark mods from ***REMOVED*** as isMod and obey them 
 # TODO login without connection details in source code
 # TODO check that the url is on r/france
 # TODO define methods using def
 # TODO define method report fault to ***REMOVED***
+# TODO define method report things to mods
 # TODO define method redditor.HasEnoughKarma(amountOfKarmaNeeded)
 # TODO define method redditor.IsTrusted() returning bool
 # TODO improve fault reporting returning error to ***REMOVED***
@@ -56,6 +57,7 @@ while True:
         message_content = ""
         spacing = " - "
         senderIsTrusted = False
+        senderIsMod = False
         senderKarma = 0
         senderName = ""
         # get sender name
@@ -72,12 +74,17 @@ while True:
             if senderName == user.name:
                 senderIsTrusted = True
                 break
+        for moderator in reddit.subreddit("***REMOVED***").moderator():
+           # print(f"{moderator}: {moderator.mod_permissions}")
+            if senderName == moderator.name:
+                senderIsMod = True
+                break
         # ignore comments
         if message.was_comment:
             message.mark_read()
         else:
             # admin command 1 Trust
-            if title == "Trust" and senderIsTrusted:
+            if title == "Trust" and senderIsMod:
                 try:
                     reddit.redditor(body).trust()
                     message.mark_read()
@@ -87,7 +94,7 @@ while True:
                     message.reply("il y a eu un problème, u/***REMOVED*** a été informé")
                     message.mark_read()
             # admin command 2 Distrust
-            elif title == "Distrust" and senderIsTrusted:
+            elif title == "Distrust" and senderIsMod:
                 try:
                     reddit.redditor(body).distrust()
                     message.mark_read()
@@ -97,7 +104,7 @@ while True:
                     message.reply("il y a eu un problème, u/***REMOVED*** a été informé")
                     message.mark_read()
             # admin command 3 Block
-            elif title == "Block" and senderIsTrusted:
+            elif title == "Block" and senderIsMod:
                 try:
                     reddit.redditor(body).block()
                     message.mark_read()
@@ -107,7 +114,7 @@ while True:
                     message.reply("il y a eu un problème, u/***REMOVED*** a été informé")
                     message.mark_read()
             # admin command 4 unblock
-            elif title == "Unblock" and senderIsTrusted:
+            elif title == "Unblock" and senderIsMod:
                 try:
                     reddit.redditor(body).unblock()
                     message.mark_read()
@@ -119,7 +126,13 @@ while True:
                 # message.mark_read()
             elif senderKarma > minKarma:
                 try:
-                    if " " in body:
+                    if "np.reddit.com/r/***REMOVED***" in body:
+                    # elif "np.reddit.com/r/france" in body:
+                        reddit.subreddit("***REMOVED***").submit(title, url=body)
+                        reddit.subreddit("***REMOVED***").message(senderName + " vient de poster sur r/***REMOVED***: ", message_content)
+                        message.mark_read()
+                    else:
+                    # elif " " in body:
                         # reddit.subreddit("***REMOVED***").submit(title, selftext=body)
                         # message_content = message_content + body
                         message.reply("Ce bot n'accepte actuellement que les message dont le corps est un lien vers r/France. Merci d'envoyer un nouveau message ayant pour objet le titre souhaité pour le post et pour corps le lien vers r/France")
@@ -127,11 +140,6 @@ while True:
                         reddit.redditor("***REMOVED***").message("posting to ***REMOVED***", message_content)   
                         # reddit.redditor("***REMOVED***").message("ISSUE WITH BOT UNBLOCKING", message_content)
                         # message.reply("il y a eu un problème, u/***REMOVED*** a été informé")       
-                        message.mark_read()
-                    elif "np.reddit.com/r/***REMOVED***" in body:
-                    # elif "np.reddit.com/r/france" in body:
-                        reddit.subreddit("***REMOVED***").submit(title, url=body)
-                        reddit.subreddit("***REMOVED***").message(senderName + " vient de poster sur r/***REMOVED***: ", message_content)
                         message.mark_read()
                     # elif "reddit" in body
                     # elif "/r/france" in body
