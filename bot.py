e76578d3d54b98fbae44490a75b5f8fa07e435f6
Damien Rembert <damien.rembert@gmail.com>
2021-07-17 03:06:45 +0100

    final maybe

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ trustWord = "Trust"
 distrustWord = "Distrust"
 adminWordList = [helpWord, blockWord, unblockWord, trustWord, distrustWord]
 
-helpMessage = "Bonjour,\nEn tant que mod de r/***REMOVED***, vous pouvez utiliser plusieurs fonctions spéciales de ce bot.\nPour cela il suffit d'envoyer un message à ce bot avec pour objet:\n" + helpWord + ", pour recevoir ce message, qui définit les différentes options.\nLes autres options servent à la gestion des redditeur qui utilisent le bot. Ces fonctions s'utilisent en mettant un mot-clé en objet (première lettre majuscule et le reste en minuscule) et le nom du redditeur (sans /u/) dans le corps du message.\n" + trustWord + ", pour ajouter quelqu'un à la liste des Redditors autorisés à poster sans signalement au modmail.\n" + distrustWord + " pour retirer une personne de cette liste.\n" + blockWord + ", pour ajouter un redditeur à la liste de spam du bot et que ses messages soient refusés automatiquement.\n" + unblockWord + ", pour retirer une personne de cette liste.\n\nCe bot a été crée par /u/***REMOVED***, n'hésitez pas à le contacter au besoin!"
+helpMessage = "Bonjour,\nEn tant que mod de r/***REMOVED***, vous pouvez utiliser plusieurs fonctions spéciales de ce bot.\nPour cela il suffit d'envoyer un message à ce bot avec pour objet:\n" + helpWord + ", pour recevoir ce message, qui définit les différentes options.\nLes autres options servent à la gestion des redditeur qui utilisent le bot. Ces fonctions s'utilisent en mettant un mot-clé en objet (première lettre majuscule et le reste en minuscule) et le nom du redditeur (sans /u/) dans le corps du message.\n" + trustWord + ", pour ajouter quelqu'un à la liste des Redditors autorisés à poster sans signalement au modmail.\n" + distrustWord + " pour retirer une personne de cette liste.\n" + blockWord + ", pour ajouter un redditeur à la liste de spam du bot et que ses messages soient refusés automatiquement.\n" + unblockWord + ", pour retirer une personne de cette liste.\n\nCe bot a été crée par /u/***REMOVED***, n'hésitez pas à le contacter au besoin!\n\n\n"
 
 
 # put try/except at lowest level only
@@ -184,7 +184,7 @@ while True:
                 message.mark_read()
                 break
             # admin command 1 Trust
-            if title == "Trust":
+            elif title == "Trust":
                 if isTrusted(body):
                     message.reply(body + " est déjà sur la liste des redditeurs approuvés.\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
                     message.mark_read()
@@ -194,16 +194,17 @@ while True:
                         reddit.redditor(body).trust()
                         refreshList("trusted")
                         message.reply(body + " est maintenant sur la liste des redditeurs approuvés.\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
-                        reddit.subreddit(selectedSub).message(senderName + " vient d'ajouter " + body + " à la liste des redditeurs approuvés." , "Voici la liste des redditeurs approuvés:\n" + trustedList +  helpSuggestion)
+                        reddit.subreddit(selectedSub).message(senderName + " vient d'ajouter " + body + " à la liste des redditeurs approuvés." , "Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
                         message.mark_read()
                         break
                     except:
-                        message.reply("Trust n'a pas fonctionné. " + body + " est peut-être déjà sur la liste des redditeurs approuvés?\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
+                        reddit.redditor("***REMOVED***").message(sender + " a envoyé un message qui a rencontré une erreur", message_content)
+                        # message.reply("Trust n'a pas fonctionné. " + body + " est peut-être déjà sur la liste des redditeurs approuvés?\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
                         message.mark_read()
                         break
 
-            # admin command 2 Distrust trial
-            if title == "Distrust":
+            # admin command 2 Distrust
+            elif title == "Distrust":
                 if not isTrusted(body):
                     message.reply(body + " n'est pas sur la liste des redditeurs approuvés.\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
                     message.mark_read()
@@ -213,83 +214,88 @@ while True:
                         reddit.redditor(body).distrust()
                         refreshList("trusted")
                         message.reply(body + " est maintenant supprimé de la liste des redditeurs approuvés.\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
-                        reddit.subreddit(selectedSub).message(senderName + " vient d'ajouter " + body + " à la liste des redditeurs approuvés." , " Voici la liste des redditeurs approuvés:\n" + trustedList +  helpSuggestion)
+                        reddit.subreddit(selectedSub).message(senderName + " vient d'ajouter " + body + " à la liste des redditeurs approuvés." , " Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
                         message.mark_read()
                         break
                     except:
-                        message.reply("Trust n'a pas fonctionné. " + body + " est peut-être déjà sur la liste des redditeurs approuvés?\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
+                        reddit.redditor("***REMOVED***").message(sender + " a envoyé un message qui a rencontré une erreur", message_content)
+                        # message.reply("Trust n'a pas fonctionné. " + body + " est peut-être déjà sur la liste des redditeurs approuvés?\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
                         message.mark_read()
                         break
                         
             # admin command 3 Block
-            elif title == "Block" and senderIsMod:
-                try:
-                    # is target already blocked
-                    for user in blockedList:
-                        if body == user.name:
-                            targetIsBlocked = True
-                        break
-                    if targetIsBlocked:
-                        message.reply(body + " est déjà sur la liste des redditeurs bloqués.")
-                    else:
+            elif title == "Block":
+                if isBlocked(body):
+                    message.reply(body + " est déjà sur la liste des redditeurs bloqués.\n Voici la liste des redditeurs bloqués:\n" + listToString(blockedList) +  helpSuggestion)
+                    message.mark_read()
+                    break
+                else:
+                    try:
                         reddit.redditor(body).block()
-                        message.reply(body + " est maintenant sur la liste des redditeurs bloqués.")
-                    message.mark_read()
-                except:
-                    reddit.redditor("***REMOVED***").message("ISSUE WITH BOT BLOCKING", message_content)
-                    message.reply("il y a eu un problème, u/***REMOVED*** a été informé")
-                    message.mark_read()
-            # admin command 4 unblock
-            elif title == "Unblock" and senderIsMod:
-                try:
-                    # is target blocked
-                    for user in blockedList:
-                        if body == user.name:
-                            targetIsBlocked = True
+                        refreshList("blocked")
+                        message.reply(body + " est maintenant sur la liste des redditeurs bloqués.\n Voici la liste des redditeurs bloqués:\n" + listToString(blockedList) +  helpSuggestion)
+                        reddit.subreddit(selectedSub).message(senderName + " vient d'ajouter " + body + " à la liste des redditeurs bloqués." , "Voici la liste des redditeurs bloqués:\n" + listToString(blockedList) +  helpSuggestion)
+                        message.mark_read()
                         break
-                    if targetIsBlocked:
+                    except:
+                        reddit.redditor("***REMOVED***").message(sender + " a envoyé un message qui a rencontré une erreur", message_content)
+                        # message.reply("Trust n'a pas fonctionné. " + body + " est peut-être déjà sur la liste des redditeurs approuvés?\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
+                        message.mark_read()
+                        break
+
+            # admin command 4 unblock
+            elif title == "Unblock":
+                if not isBlocked(body):
+                    message.reply(body + " n'est pas sur la liste des redditeurs bloqués.\n Voici la liste des redditeurs bloqués:\n" + listToString(blockedList) +  helpSuggestion)
+                    message.mark_read()
+                    break
+                else:
+                    try:
                         reddit.redditor(body).unblock()
-                        message.reply(body + " n'est plus sur la liste des redditeurs bloqués.")
-                    else:
-                        message.reply(body + " n'était pas sur la liste des redditeurs bloqués.")
-                    message.mark_read()
-                except:
-                    reddit.redditor("***REMOVED***").message("ISSUE WITH BOT UNBLOCKING", message_content)
-                    message.reply("il y a eu un problème, u/***REMOVED*** a été informé")
-                    message.mark_read()
+                        refreshList("blocked")
+                        message.reply(body + " n'est plus sur la liste des redditeurs bloqués.\n Voici la liste des redditeurs bloqués:\n" + listToString(blockedList) +  helpSuggestion)
+                        reddit.subreddit(selectedSub).message(senderName + " vient d'ajouter " + body + " à la liste des redditeurs bloqués." , "Voici la liste des redditeurs bloqués:\n" + listToString(blockedList) +  helpSuggestion)
+                        message.mark_read()
+                        break
+                    except:
+                        reddit.redditor("***REMOVED***").message(sender + " a envoyé un message qui a rencontré une erreur", message_content)
+                        # message.reply("Trust n'a pas fonctionné. " + body + " est peut-être déjà sur la liste des redditeurs approuvés?\n Voici la liste des redditeurs approuvés:\n" + listToString(trustedList) +  helpSuggestion)
+                        message.mark_read()
+                        break
+
                 # message.mark_read()
-            elif senderKarma > minKarma:
-                try:
-                    if " " in body:
-                        # reddit.subreddit(selectedSub).submit(title, selftext=body)
-                        message.reply("Ce bot n'accepte actuellement que les message dont le corps contient uniquement un lien vers un post ou un commentaire sur r/France. Merci d'envoyer un nouveau message ayant pour objet le titre souhaité pour le post et pour corps un lien vers r/France")
-                        reddit.subreddit(selectedSub).message("Post refusé, le corps du message contient un caractère interdit: " +senderName + " vient d'essayer de poster anonymement sur r/" + selectedSub + ":", message_content)
-                        message.mark_read()
-                    elif "r/***REMOVED***/" in body:
-                    # elif "r/france/" in body:
-                        reddit.subreddit(selectedSub).submit(title, url=cleanUrl(body))
-                        reddit.subreddit(selectedSub).message(senderName + " vient de poster sur r/" + selectedSub + ":", message_content)
-                        message.reply("Merci, votre message devrait apparaître sur r/***REMOVED*** dans moins d'une minute!")
-                        message.mark_read()
-                    else:
+        elif senderKarma >= minKarma:
+            try:
+                # if " " in body:
+                #         # reddit.subreddit(selectedSub).submit(title, selftext=body)
+                #         message.reply("Ce bot n'accepte actuellement que les message dont le corps contient uniquement un lien vers un post ou un commentaire sur r/France. Merci d'envoyer un nouveau message ayant pour objet le titre souhaité pour le post et pour corps un lien vers r/France")
+                #         reddit.subreddit(selectedSub).message("Post refusé, le corps du message contient un caractère interdit: " +senderName + " vient d'essayer de poster anonymement sur r/" + selectedSub + ":", message_content)
+                #         message.mark_read()
+                if "r/***REMOVED***/" in body:
+                # elif "r/france/" in body:
+                    reddit.subreddit(selectedSub).submit(title, url=cleanUrl(body))
+                    reddit.subreddit(selectedSub).message(senderName + " vient de poster sur r/" + selectedSub + ":", message_content)
+                    message.reply("Merci, votre message devrait apparaître sur r/***REMOVED*** dans moins d'une minute!")
+                    message.mark_read()
+                    break
+                else:
                     # elif " " in body:
                         # reddit.subreddit(selectedSub).submit(title, selftext=body)
-                        message.reply("Ce bot n'accepte actuellement que les message dont le corps contient uniquement un lien vers un post ou un commentaire sur r/France. Merci d'envoyer un nouveau message ayant pour objet le titre souhaité pour le post et pour corps un lien vers r/France")
-                        reddit.subreddit(selectedSub).message("Post refusé: le corps du message ne semble pas contenir de lien vers r/france. " + senderName + " vient d'essayer de poster anonymement sur r/" + selectedSub + ":", message_content)
-                        message.mark_read()
+                    message.reply("Ce bot n'accepte actuellement que les message dont le corps contient uniquement un lien vers un post ou un commentaire sur r/France. Merci d'envoyer un nouveau message ayant pour objet le titre souhaité pour le post et pour corps un lien vers r/France")
+                    reddit.subreddit(selectedSub).message("Post refusé: le corps du message ne semble pas contenir de lien vers r/france. " + senderName + " vient d'essayer de poster anonymement sur r/" + selectedSub + ":", message_content)
+                    message.mark_read()
+                    break
                     # elif "reddit" in body
                     # elif "/r/france" in body
                 except:
-                    reddit.redditor("***REMOVED***").message("ISSUE WITH BOT POSTING", message_content)
+                    reddit.redditor("***REMOVED***").message(sender + " a envoyé un message qui a rencontré une erreur", message_content)
                     message.mark_read()
-            elif senderKarma <= minKarma:
-                # reddit.redditor("***REMOVED***").message(senderName + " n'a pas assez de karma - contrôler et poster", message_content)
-                message.reply("Votre karma n'est pas assez élevé, votre message doit donc être approuvé par la modération de /r/***REMOVED***. Merci de patienter un peu!")
-                reddit.subreddit(selectedSub).message("Karma de " + senderName + " trop bas, message non posté (à contrôler et poster pour ce redditeur?) - " + senderName + " vient d'essayer de poster sur r/" + selectedSub + ":", message_content)
-                message.mark_read()
+                    break
+        elif senderKarma < minKarma:
+            # reddit.redditor("***REMOVED***").message(senderName + " n'a pas assez de karma - contrôler et poster", message_content)
+            message.reply("Votre karma n'est pas assez élevé, votre message doit donc être approuvé par la modération de /r/***REMOVED***. Merci de patienter un peu!")
+            reddit.subreddit(selectedSub).message("Karma de " + senderName + " trop bas, message non posté (à contrôler et poster pour ce redditeur?) - " + senderName + " vient d'essayer de poster sur r/" + selectedSub + ":", message_content)
+            message.mark_read()
+
     # sleep one minute
     time.sleep(30)
-    # reddit.subreddit(selectedSub).message("essai modmail", "avec un objet et un corps")
-    # reddit.subreddit(selectedSub).message("essai pied" ,  helpSuggestion)
-    # reddit.subreddit(selectedSub).message(senderName + " vient de poster sur r/" + selectedSub + ":", message_content)
-
