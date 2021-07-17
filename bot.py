52e9e3b60038a493e5f50a322c43961562774e77
Damien Rembert <damien.rembert@gmail.com>
2021-07-17 17:32:33 +0100

    trying to fix issue with non url posts

--- bot.py
+++ bot.py
@@ -311,14 +311,16 @@ while True:
         elif senderKarma >= minKarma:
             try:
                 if "r/***REMOVED***/" in body:
-                # elif "r/france/" in body:
-                    reddit.subreddit(selectedSub).submit(title, url=cleanUrl(body))
+                # if "r/france/" in body:
+                    cleanedUrl = cleanUrl(body)
+                    reddit.subreddit(selectedSub).submit(title, url=cleanedUrl)
                     reddit.subreddit(selectedSub).message(senderName + " vient de poster sur r/" + selectedSub + ":", message_content + helpSuggestion)
                     message.reply("Merci, votre message devrait apparaître sur r/***REMOVED*** dans moins d'une minute!")
                     message.mark_read()
                     break
                 else:
-                    message.reply("Ce bot n'accepte actuellement que les message dont le corps contient uniquement un lien vers un post ou un commentaire sur r/France. Merci d'envoyer un nouveau message ayant pour objet le titre souhaité pour le post et pour corps un lien vers r/France")
+                    message.reply("Ce bot n'accepte actuellement que les message dont le corps contient uniquement un lien vers un post ou un commentaire sur r/France.\n\n\nMerci d'envoyer un nouveau message ayant pour objet le titre souhaité pour le post et pour corps un lien vers r/France")
+                    reddit.subreddit(selectedSub).message(senderName + " vient d'essayer de poster sur r/" + selectedSub + ":", message_content + helpSuggestion)
                     reddit.subreddit(selectedSub).message("Post refusé: le corps du message ne semble pas contenir de lien vers r/france. " + senderName + " vient d'essayer de poster anonymement sur r/" + selectedSub + ":", message_content + helpSuggestion)
                     message.mark_read()
                     break
